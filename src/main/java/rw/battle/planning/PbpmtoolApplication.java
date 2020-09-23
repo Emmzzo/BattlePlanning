8c6548a1d49fab1897a94b96634669a89f23345b
EmmaSibo <sibo@2019gitlab>
2020-09-23 11:25:53 +0200

    Initialize project to git repo

---
+++ src/main/java/rw/battle/planning/PbpmtoolApplication.java
@@ -0,0 +1,13 @@
+package rw.battle.planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbpmtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/battle/planning/PbpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.battle.planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
