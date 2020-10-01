55422c532947ff8602fa218fff35a929020a32f3
EmmaSibo <sibo@2019gitlab>
2020-10-01 10:14:08 +0200

    React client build package

---
+++ src/main/resources/static/precache-manifest.1de0c20470ea9e131ef48d9cf811e2b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf86bda12108029639ef07a87fd34b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04757d280b3f703e8a72",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e3d3824b022ddbd58984",
+    "url": "/static/css/main.72e8e562.chunk.css"
+  },
+  {
+    "revision": "04757d280b3f703e8a72",
+    "url": "/static/js/2.3aa96f76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3aa96f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d3824b022ddbd58984",
+    "url": "/static/js/main.72a6b52d.chunk.js"
+  },
+  {
+    "revision": "40843942cc763ae8c518",
+    "url": "/static/js/runtime-main.fbb2370e.js"
+  }
+]);
